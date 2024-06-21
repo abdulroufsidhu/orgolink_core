85a8faa5cac868d5554b7b80b9d78708088a5df6
Abdul Rauf <abdulroufsidhu@gmail.com>
2024-06-21 22:40:44 +0500

    allowed cors origin for user requests

--- src/main/kotlin/io/github/abdulroufsidhu/ambaar/user/UserRequests.kt
+++ src/main/kotlin/io/github/abdulroufsidhu/ambaar/user/UserRequests.kt
@@ -3,15 +3,12 @@ package io.github.abdulroufsidhu.ambaar.user
 import io.github.abdulroufsidhu.ambaar.core.Responser
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -26,12 +23,16 @@ class UserRequests(
             userLogic.createUser(user)
         }
 
+    data class SignInRequstData(
+        val email: String,
+        val password: String,
+    )
+
     @PostMapping("/sign-in")
     fun signIn(
-        @RequestParam("email") email: String,
-        @RequestParam("password") password: String,
+        @RequestBody signInRequstData: SignInRequstData,
     ): Responser<Responser.ResponseObj<User>> = Responser.success {
-        userLogic.signIn(email, password)
+        userLogic.signIn(signInRequstData.email, signInRequstData.password)
     }
 
     @PatchMapping("/update-password/{id}")
